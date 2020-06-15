eb1064ce51a33306b17023bc534b5b964e49f1f6
Tyler Boyd <tylerb@theboydprojects.com>
2020-06-15 12:36:53 -0500

    initial commit

---
+++ ormconfig.js
@@ -0,0 +1,23 @@
+require('dotenv/config');
+
+const { DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_HOST } = process.env;
+
+module.exports = [{
+    name: 'default',
+    type: 'postgres',
+    host: DB_HOST,
+    port: DB_PORT,
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+    synchronise: true,
+    entities: [
+        'src/**.module/*-module.ts'
+    ],
+    subscribers: [
+        'src/**.module/*-subscriber.ts'
+    ],
+    migrations: [
+        'src/migrations/*.ts'
+    ]
+}];
